24a433c61c60c56343549bc7e5e79e2c34261652
Andrew Buchan <andyhasit@gmail.com>
2016-04-03 16:52:11 +0100

    Basic 'add' functionality + test

--- shopping_basket/logic.py
+++ shopping_basket/logic.py
@@ -1,7 +1,62 @@
-class ShoppingBasket(object):
+
+
+class Product(object):
     """
-    An individual shopping backet. Contains items.
+    A product in the shop.
+    """
+    def __init__(self, description, price):
+        self.description = description
+        self.price = description
+    
+    
+class BasicShoppingBasket(object):
+    """
+    A basic shopping backet which allows adding and removing of items.
     """
     
     def __init__(self):
-        pass
+        self._items = []
+    
+    def add_item(self, product, quantity):
+        """
+        Adds an entry in the basket, or increments the quantity if the product
+        is already in the basket.
+        """
+        existing_entry_for_product = self.get_item_in_basket(product)
+        if existing_entry_for_product:
+            new_quantity = existing_entry_for_product.quantity + quantity
+            existing_entry_for_product.update_quantity(new_quantity)
+        else:
+            self._items.append(ItemInBasket(product, quantity))
+        
+    def get_items(self):
+        """
+        Returns a list of the items.
+        Note this is a copy, so manilpulating the returned list has no effect on
+        the shopping basket.
+        """
+        return [i for i in self._items]
+        
+    def get_item_in_basket(self, product):
+        """
+        
+        """
+        
+class ItemInBasket(object):
+    """
+    An item  in the shopping basket.
+    """
+    def __init__(self, product, quantity):
+        self._product = product
+        self._quantity = quantity
+    
+    @property
+    def description(self):
+        return self._product.description
+    
+    @property
+    def quantity(self):
+        return self._quantity
+        
+    def update_quantity(self, new_quantity):
+        self._quantity = new_quantity
--- shopping_basket/logic_test.py
+++ shopping_basket/logic_test.py
@@ -1,5 +1,16 @@
 from logic import * 
 
-def test_mm():
-    #a = 
-    assert 1 == 4
+
+def test_add_one_item():
+    apple = Product('apple', 2)
+    orange = Product('orange', 2.50)
+    basket = BasicShoppingBasket()
+    basket.add_item(apple, 4)
+    items = basket.get_items()
+    assert len(items) == 1
+    item1 = items[0]
+    assert item1.description == 'apple'
+    assert item1.quantity == 4
+    
+    
+    
