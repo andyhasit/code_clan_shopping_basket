c6e1dd68bbfed926d976ed917c14700bc4218600
Andrew Buchan <andyhasit@gmail.com>
2016-04-03 17:31:45 +0100

    Created controller which only allows unique product descriptions

--- shopping_basket/logic.py
+++ shopping_basket/logic.py
@@ -1,10 +1,41 @@
 
-
+class Controller(object):
+    """
+    The main controller for the application.
+    This is the only object the UI has a reference to (initially).
+    """
+    def __init__(self):
+        self._products = []
+        self._used_descriptions = []
+        self._unique_desc_error_message = \
+                "Description ${0} is already claimed by an other product"
+        
+    def get_products(self):
+        return self._products[:]
+    
+    def new_product(self, description, price):
+        if description in self._used_descriptions:
+            raise ValueError(self._unique_desc_error_message.format(description))
+        self._used_descriptions.append(description)
+        self._products.append(Product(description, price))
+        
+    def new_basket(self, name):
+        """
+        Returns a new shopping basket.
+        This is where we set up 
+        """
+        basic_basket = BasicShoppingBasket()
+        extended_basket = BasketWithDiscounts(basic_basket)
+        extended_basket.add_price_adjuster()
+        return extended_basket
+        
 class Product(object):
     """
     A product in the shop. Descriptions must be unique.
     """
+    
     def __init__(self, description, price):
+        
         self._description = description
         self._price = price
     
@@ -40,11 +71,22 @@ class BasicShoppingBasket(object):
     def get_items(self):
         """
         Returns a list of the items.
-        Note this is a copy, so manipulating the returned list has no effect on
+        Note: this is a copy, so manipulating the returned list has no effect on
         the shopping basket.
         """
-        return [i for i in self._items]
+        return self._items[:]
+    
+    def remove_item(self, item_in_basket):
+        self._items.remove(item_in_basket)
         
+    def empty(self):
+        """
+        Empties the list.
+        Note: retains the same list object, in case that ends up being tracked
+        elsewhere (e.g UI databinding)
+        """
+        del self._items[:]
+            
     def get_item_in_basket(self, product):
         """
         Returns the entry for the specified product, or None.
